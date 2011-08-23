eacd738eb6199bc09e319611e973df761ca93ef1
Rowan Manning <rowan@rowanmanning.co.uk>
2011-08-23 22:09:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+
+# Topsy API PHP Wrapper
+
+This is a simple (Work-In-Progress) PHP wrapper for the Topsy API. The library requires PHP 5.3, and is easy to use.
+
+Read the [API Documentation](http://code.google.com/p/otterapi/w/list) for more info on the API itself, or see examples below for using the library.
+
+The library implements Topsy's API keys, which are not quite live, but it's best to send one anyway in preparation. Topsy also asks that you send a User-Agent containing the URL of your app or service, this can be done in the constructor.
+
+## Basic Usage
+
+The `get` method is used to send a `GET` request to the Topsy API. The first argument is the endpoint to get, the second is an array of query parameters. All class methods are well documented in the source if you require further detail.
+
+	<?php
+	
+	// config
+	$api_key = 'ABC123';
+	$user_agent = 'http://yourapp.com/';
+	
+	// create a new `Topsy` instance
+	$topsy = new Topsy($api_key, $user_agent);
+	
+	// make a call to the '/trackbacks' endpoint, getting trackbacks for the GitHub website
+	$topsy->get('trackbacks', array(
+		'url' => 'http://github.com/',
+	));
+	
+	?>
+
+Most API endpoints have shortcut functions too.
+
+	<?php
+	
+	// we can simplify the above example like this
+	$topsy->trackbacks('http://github.com/');
+	
+	?>
+
+---
+
+Copyright 2011, Rowan Manning  
+Dual licensed under the MIT or GPL Version 2 licenses.
---
+++ lib/Topsy.php
@@ -0,0 +1,460 @@
+<?php
+/**
+ * Topsy API Class.
+ *
+ * @author Rowan Manning <info@rowanmanning.co.uk>
+ * @copyright Copyright 2011, Rowan Manning
+ * @license Dual licensed under the MIT or GPL Version 2 licenses.
+ * @filesource
+ */
+
+//============================================================
+// CLASS DEFINITION
+//============================================================
+
+/**
+ * Topsy API Class.
+ */
+class Topsy {
+	
+	//------------------------------------------------------------
+	// VARIABLES
+	//------------------------------------------------------------
+	
+	/**
+	 * The API key to send with all API requests.
+	 *
+	 * @var string
+	 */
+	protected $api_key = null;
+	
+	/**
+	 * The HTTP User-Agent header to send with all API requests.
+	 *
+	 * @var string
+	 */
+	protected $user_agent = null;
+	
+	/**
+	 * The API rate limit.
+	 *
+	 * @var integer
+	 */
+	protected $api_rate_limit = null;
+	
+	/**
+	 * The API rate limit remaining.
+	 *
+	 * @var integer
+	 */
+	protected $api_rate_limit_remaining = null;
+	
+	/**
+	 * The API rate limit reset time, as a UNIX time-stamp.
+	 *
+	 * @var integer
+	 */
+	protected $api_rate_limit_reset = null;
+	
+	//------------------------------------------------------------
+	// GET/SET FUNCTIONS
+	//------------------------------------------------------------
+	
+	/**
+	 * Get/set the API key that is sent with all API requests.
+	 *
+	 * @param string $value The API key to set. Default value is `null`.
+	 * @return string|Topsy Returns the API key if `$value` is `null`, otherwise the calling `Topsy` object is returned for chaining.
+	 */
+	public function api_key($value = null) {
+		
+		// get
+		if ($value === null) {
+			return $this->api_key;
+		}
+		
+		// set
+		$this->api_key = (string) $value;
+		return $this;
+		
+	}
+	
+	/**
+	 * Get/set the HTTP User-Agent header that is sent with all API requests.
+	 *
+	 * @param string $value The HTTP User-Agent to set. Topsy specifies that this should be your application or service URL. Default value is `null`.
+	 * @return string|Topsy Returns the User-Agent if `$value` is `null`, otherwise the calling `Topsy` object is returned for chaining.
+	 */
+	public function user_agent($value = null) {
+		
+		// get
+		if ($value === null) {
+			return $this->user_agent;
+		}
+		
+		// set
+		$this->user_agent = (string) $value;
+		return $this;
+		
+	}
+	
+	/**
+	 * Get the API rate limit.
+	 *
+	 * @return integer Returns the API rate limit.
+	 */
+	public function api_rate_limit() {
+		
+		// ensure we have rate limit data
+		if ($this->api_rate_limit === null) {
+			$this->get('credit');
+		}
+		
+		// get
+		return $this->api_rate_limit;
+		
+	}
+	
+	/**
+	 * Get the API rate limit remaining.
+	 *
+	 * @return integer Returns the API rate limit remaining.
+	 */
+	public function api_rate_limit_remaining() {
+		
+		// ensure we have rate limit data
+		if ($this->api_rate_limit === null) {
+			$this->get('credit');
+		}
+		
+		// get
+		return $this->api_rate_limit_remaining;
+		
+	}
+	
+	/**
+	 * Get the API rate limit reset time, as a UNIX time-stamp.
+	 *
+	 * @return integer Returns the API rate limit reset time.
+	 */
+	public function api_rate_limit_reset() {
+		
+		// ensure we have rate limit data
+		if ($this->api_rate_limit === null) {
+			$this->get('credit');
+		}
+		
+		// get
+		return $this->api_rate_limit_reset;
+		
+	}
+	
+	//------------------------------------------------------------
+	// CORE FUNCTIONS
+	//------------------------------------------------------------
+	
+	/**
+	 * Class constructor.
+	 * 
+	 * @param string $api_key The API key to send with all API requests.
+	 * @param string $user_agent The HTTP User-Agent header to send with all API requests. Topsy specifies that this should be your application or service URL.
+	 */
+	public function __construct($api_key = null, $user_agent = null) {
+		
+		// sanitize and store arguments
+		$this->api_key = (string) $api_key;
+		$this->user_agent = (string) $user_agent;
+		
+	}
+	
+	/**
+	 * Calculate an API URL from an endpoint.
+	 *
+	 * @param string $value The API key to set. Default value is `null`.
+	 * @return string|Topsy Returns the API key if `$value` is `null`, otherwise the calling `Topsy` object is returned for chaining.
+	 */
+	protected function calculate_url($endpoint) {
+		
+		// sanitize arguments
+		$endpoint = trim((string) $endpoint, '/');
+		
+		// return the URL
+		return 'http://otter.topsy.com/' . $endpoint . '.json';
+		
+	}
+	
+	/**
+	 * Make a request to the API.
+	 *
+	 * @param string $method The HTTP method to use.
+	 * @param string $endpoint The API endpoint to request.
+	 * @param array $params An array of parameters to send with the request. Default value is `null`.
+	 * @return array Returns an array of response data.
+	 */
+	protected function request($method, $endpoint, $params = null) {
+		
+		// sanitize arguments
+		$method = strtoupper((string) $method);
+		$params = ($params === null ? array() : (array) $params);
+		
+		// check for cURL
+		if (!function_exists('curl_init')) {
+			throw new RuntimeException('The PHP cURL extension is required to make API requests.');
+		}
+		
+		// calculate the URL
+		$url = $this->calculate_url($endpoint);
+		
+		// store API key and build the query
+		if ($this->api_key !== null) {
+			$params['apikey'] = $this->api_key;
+		}
+		$query = http_build_query($params);
+		
+		// initialize a cURL handle and set some basic options
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HEADER, true);
+		
+		// set headers
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			'User-Agent: ' . $this->user_agent,
+		));
+		
+		// set method and query/body
+		if ($method == 'GET') {
+			$url = $url . '?' . $query;
+		} else {
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
+		}
+		
+		// set the cURL URL
+		curl_setopt($ch, CURLOPT_URL, $url);
+		
+		// execute the request
+		$raw_response = curl_exec($ch);
+		$curl_info = curl_getinfo($ch);
+		curl_close($ch);
+		
+		// get the response headers/body
+		@list($raw_response_headers, $response_body) = preg_split('/(\r\n|\n){2}/', $raw_response, 2);
+		$raw_response_headers = preg_split('/(\r\n|\n)/', $raw_response_headers);
+		array_shift($raw_response_headers);
+		$response_headers = array();
+		foreach ($raw_response_headers as $response_header) {
+			$response_header_name = $response_header_value = '';
+			@list($response_header_name, $response_header_value) = explode(':', $response_header, 2);
+			$response_headers[trim($response_header_name)] = trim($response_header_value);
+		}
+		
+		// store the new rate limit data
+		if (isset($response_headers['X-RateLimit-Limit'])) {
+			$this->api_rate_limit = (integer) $response_headers['X-RateLimit-Limit'];
+			$this->api_rate_limit_remaining = (integer) $response_headers['X-RateLimit-Remaining'];
+			$this->api_rate_limit_reset = (integer) $response_headers['X-RateLimit-Reset'];
+		}
+		
+		// package up and return the response
+		$response = (object) array(
+			'status' => $curl_info['http_code'],
+			'headers' => $response_headers,
+			'body' => json_decode($response_body),
+		);
+		return $response;
+		
+	}
+	
+	/**
+	 * Make a GET request to the API.
+	 *
+	 * @param string $endpoint The API endpoint to request.
+	 * @param array $params An array of parameters to send with the request. Default value is `null`.
+	 * @return array Returns an array of response data.
+	 */
+	public function get($endpoint, $params = null) {
+		
+		return $this->request('GET', $endpoint, $params);
+		
+	}
+	
+	//------------------------------------------------------------
+	// API ENDPOINT SHORTCUT FUNCTIONS
+	//------------------------------------------------------------
+	
+	/**
+	 * Get profile information for an author.
+	 *
+	 * See http://code.google.com/p/otterapi/wiki/Resources#/authorinfo for more information.
+	 * 
+	 * @param string $author_url URL string for the author.
+	 * @param array $params An array of additional parameters to send with the request. Default value is `null`.
+	 * @return array Returns an array of response data.
+	 */
+	public function authorinfo($author_url, $params = null) {
+		
+		$params['url'] = $author_url;
+		return $this->get('authorinfo', $params);
+		
+	}
+	
+	/**
+	 * Get a list of authors who talk about a query.
+	 *
+	 * See http://code.google.com/p/otterapi/wiki/Resources#/experts for more information.
+	 * 
+	 * @param string $query Search query string. See http://code.google.com/p/otterapi/wiki/QuerySyntax for syntax.
+	 * @param array $params An array of additional parameters to send with the request. Default value is `null`.
+	 * @return array Returns an array of response data.
+	 */
+	public function experts($query, $params = null) {
+		
+		$params['q'] = $query;
+		return $this->get('experts', $params);
+		
+	}
+	
+	/**
+	 * Get a list of URLs posted by an author.
+	 *
+	 * See http://code.google.com/p/otterapi/wiki/Resources#/linkposts for more information.
+	 * 
+	 * @param string $author_url URL string for the author.
+	 * @param string $contains Filter query string. See http://code.google.com/p/otterapi/wiki/QuerySyntax for syntax. Default value is `null`.
+	 * @param string $tracktype The type of post to retrieve. Options include: 'image', 'tweet__various' (tweets and retweets) and 'self__tweet' (tweets by user). Default value is `null`.
+	 * @param array $params An array of additional parameters to send with the request. Default value is `null`.
+	 * @return array Returns an array of response data.
+	 */
+	public function linkposts($author_url, $contains = null, $tracktype = null, $params = null) {
+		
+		$params['url'] = $author_url;
+		if ($contains !== null) {
+			$params['contains'] = $contains;
+		}
+		if ($tracktype !== null) {
+			$params['tracktype'] = $tracktype;
+		}
+		return $this->get('linkposts', $params);
+		
+	}
+	
+	/**
+	 * Get a count of URLs posted by an author.
+	 *
+	 * See http://code.google.com/p/otterapi/wiki/Resources#/linkpostcount for more information.
+	 * 
+	 * @param string $author_url URL string for the author.
+	 * @param string $contains Filter query string. See http://code.google.com/p/otterapi/wiki/QuerySyntax for syntax. Default value is `null`.
+	 * @param string $tracktype The type of post to retrieve. Options include: 'image', 'tweet__various' (tweets and retweets) and 'self__tweet' (tweets by user). Default value is `null`.
+	 * @param array $params An array of additional parameters to send with the request. Default value is `null`.
+	 * @return array Returns an array of response data.
+	 */
+	public function linkpostcount($author_url, $contains = null, $tracktype = null, $params = null) {
+		
+		$params['url'] = $author_url;
+		if ($contains !== null) {
+			$params['contains'] = $contains;
+		}
+		if ($tracktype !== null) {
+			$params['tracktype'] = $tracktype;
+		}
+		return $this->get('linkpostcount', $params);
+		
+	}
+	
+	/**
+	 * Get a list of the most popular and unique tweets (trackbacks) that mention the query URL.
+	 *
+	 * See http://code.google.com/p/otterapi/wiki/Resources#/populartrackbacks for more information.
+	 * 
+	 * @param string $url URL string for the target.
+	 * @param array $params An array of additional parameters to send with the request. Default value is `null`.
+	 * @return array Returns an array of response data.
+	 */
+	public function populartrackbacks($url, $params = null) {
+		
+		$params['url'] = $url;
+		return $this->get('populartrackbacks', $params);
+		
+	}
+	
+	/**
+	 * Get a list of related URLs.
+	 *
+	 * See http://code.google.com/p/otterapi/wiki/Resources#/related for more information.
+	 * 
+	 * @param string $url URL string for the target.
+	 * @param array $params An array of additional parameters to send with the request. Default value is `null`.
+	 * @return array Returns an array of response data.
+	 */
+	public function related($url, $params = null) {
+		
+		$params['url'] = $url;
+		return $this->get('related', $params);
+		
+	}
+	
+	/**
+	 * Get a list of results matching a query.
+	 *
+	 * See http://code.google.com/p/otterapi/wiki/Resources#/search for more information.
+	 * 
+	 * @param string $query Search query string. See http://code.google.com/p/otterapi/wiki/QuerySyntax for syntax.
+	 * @param string $window Time window for the results. See http://code.google.com/p/otterapi/wiki/Resources#/search for detail. Default value is `null`.
+	 * @param string $type The type of result to return. Supported types are 'image', 'tweet' and 'video'. Default value is `null` (all types).
+	 * @param string $query_features Enables QueryFeatures. See http://code.google.com/p/otterapi/wiki/QueryFeatures. Default value is `null`.
+	 * @param array $params An array of additional parameters to send with the request. Default value is `null`.
+	 * @return array Returns an array of response data.
+	 */
+	public function search($query, $window = null, $type = null, $query_features = null, $params = null) {
+		
+		$params['q'] = $query;
+		if ($window !== null) {
+			$params['window'] = $window;
+		}
+		if ($type !== null) {
+			$params['type'] = $type;
+		}
+		if ($query_features !== null) {
+			$params['query_features'] = $query_features;
+		}
+		return $this->get('search', $params);
+		
+	}
+	
+	/**
+	 * Get a count of results matching a query.
+	 *
+	 * See http://code.google.com/p/otterapi/wiki/Resources#/searchcount for more information.
+	 * 
+	 * @param string $query Search query string. See http://code.google.com/p/otterapi/wiki/QuerySyntax for syntax.
+	 * @param string $dynamic Whether to choose the best window for this query. Default value is `null`.
+	 * @param array $params An array of additional parameters to send with the request. Default value is `null`.
+	 * @return array Returns an array of response data.
+	 */
+	public function searchcount($query, $dynamic = null, $params = null) {
+		
+		$params['q'] = $query;
+		if ($dynamic !== null) {
+			$params['dynamic'] = $dynamic;
+		}
+		return $this->get('searchcount', $params);
+		
+	}
+	
+	// TODO
+	public function searchhistogram() {}
+	public function searchdate() {}
+	public function stats() {}
+	public function top() {}
+	public function tags() {}
+	public function trackbacks() {}
+	public function trending() {}
+	public function urlinfo() {}
+	
+	//------------------------------------------------------------
+	
+}
+
+//============================================================
+// end of file
